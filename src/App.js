4e02ba12ae01064ac2bd722d308810ec6c3cfc86
umangd98 <umangd98@gmail.com>
2020-02-21 18:55:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Dashboard from './components/Dashboard';
+import Header from './components/Layout/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AddProject from './components/Project/AddProject';
+import { Provider } from 'react-redux';
+import store from './store';
+import UpdateProject from './components/Project/UpdateProject';
+import ProjectBoard from './components/ProjectBoard/ProjectBoard';
+import AddProjectTask from './components/ProjectBoard/ProjectTask/AddProjectTask';
+import UpdateProjectTask from './components/ProjectBoard/ProjectTask/UpdateProjectTask';
+import Landing from './components/Layout/Landing';
+import Register from './components/UserManagement/Register';
+import Login from './components/UserManagement/Login';
+import jwt_decode from 'jwt-decode';
+import setJWTToken from './SecurityUtils/setJWTToken';
+import { SET_CURRENT_USER } from './actions/types';
+import { logout } from './actions/securityActions';
+import SecuredRoute from './SecurityUtils/SecureRoute';
+const jwtToken = localStorage.jwtToken;
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decode = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decode
+  });
+  const currentTime = Date.now() / 1000;
+  if (decode.exp < currentTime) {
+    //handle logout
+    store.dispatch(logout());
+    window.location.href = '/';
+  }
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          {
+            //Public Routes
+          }
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+
+          {
+            //Private Routes
+          }
+          <Switch>
+            <SecuredRoute exact path="/dashboard" component={Dashboard} />
+            <SecuredRoute exact path="/addProject" component={AddProject} />
+            <SecuredRoute
+              exact
+              path="/updateProject/:id"
+              component={UpdateProject}
+            />
+            <SecuredRoute
+              exact
+              path="/projectBoard/:id"
+              component={ProjectBoard}
+            />
+            <SecuredRoute
+              exact
+              path="/addProjectTask/:id"
+              component={AddProjectTask}
+            />
+            <SecuredRoute
+              exact
+              path="/updateProjectTask/:backlog_id/:pt_id"
+              component={UpdateProjectTask}
+            />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/components/Project/CreateProjectButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+export default function CreateProjectButton() {
+  return (
+    <React.Fragment>
+      <Link to="/addProject" className="btn btn-lg btn-info">
+        Create a Project
+      </Link>
+    </React.Fragment>
+  );
+}
